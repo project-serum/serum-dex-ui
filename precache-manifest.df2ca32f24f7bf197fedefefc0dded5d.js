c468935a4fa1132be74a0df15ce5b955e903448e
Gary Wang <gary@garywang.net>
2020-08-29 19:31:02 -0700

    Updates

---
+++ precache-manifest.df2ca32f24f7bf197fedefefc0dded5d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6daba135ab9aa30f40a5c4152b39e07a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7176ad90fde545c12bf5",
+    "url": "/static/css/2.872a6bf7.chunk.css"
+  },
+  {
+    "revision": "d5079eb2b428cee137a8",
+    "url": "/static/css/main.a14c3cbb.chunk.css"
+  },
+  {
+    "revision": "7176ad90fde545c12bf5",
+    "url": "/static/js/2.66667159.chunk.js"
+  },
+  {
+    "revision": "f337a8602d902aff8a4fcd3d87010d2a",
+    "url": "/static/js/2.66667159.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5079eb2b428cee137a8",
+    "url": "/static/js/main.d01f69cf.chunk.js"
+  },
+  {
+    "revision": "d3ebb56a3098223b5316",
+    "url": "/static/js/runtime-main.c7319a82.js"
+  },
+  {
+    "revision": "49174c736a9c871baaeb2db6d5372eee",
+    "url": "/static/media/logo.49174c73.svg"
+  }
+]);
