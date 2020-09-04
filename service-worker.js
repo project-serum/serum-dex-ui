2aa2a0d96f8b3472d33e0f6f3447f338fc07d561
Nishad <nishadsingh@gmail.com>
2020-09-04 12:14:51 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.86ffaa42029eacd783b024ca20cc7d13.js"
+  "/precache-manifest.7207cc3b295709c6439f11ea04945d95.js"
 );
 
 self.addEventListener('message', (event) => {
