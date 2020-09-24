7b7e39611980eb9eab503088899d3a52851e07da
Philippe Maes <philippe@ftx.com>
2020-09-24 15:14:55 +0200

    Fix default market naming bug

--- src/utils/markets.js
+++ src/utils/markets.js
@@ -140,7 +140,7 @@ const _SLOW_REFRESH_INTERVAL = 5 * 1000;
 const _FAST_REFRESH_INTERVAL = 1000;
 
 export const DEFAULT_MARKET = USE_MARKETS.find(
-  ({ name }) => name === 'SRM/USDT',
+  ({ name, deprecated }) => name === 'SRM/USDT' && !deprecated,
 );
 
 function getMarketDetails(market, customMarkets) {
