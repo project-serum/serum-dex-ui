4786875293bb933269bc1929f6a0ce4e6e7f5d5c
Nathaniel Parke <nathaniel.parke@gmail.com>
2020-10-13 15:11:12 +0800

    Add drop down for relevant serum academy pages

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -35,6 +35,12 @@ const LogoWrapper = styled.div`
 
 const EXTERNAL_LINKS = {
   '/learn': 'https://serum-academy.com/en/serum-dex/',
+  '/add-market': 'https://serum-academy.com/en/add-market/',
+  '/wallet-support': 'https://serum-academy.com/en/wallet-support',
+  '/dex-list': 'https://serum-academy.com/en/dex-list/',
+  '/developer-resources': 'https://serum-academy.com/en/developer-resources/',
+  '/explorer': 'https://explorer.solana.com',
+  '/srm-faq': 'https://projectserum.com/srm-faq',
 }
 
 export default function TopBar() {
@@ -129,11 +135,38 @@ export default function TopBar() {
           }}
         >
           <Menu.Item key="/">TRADE</Menu.Item>
-          <Menu.Item key="/learn">
-            <a href={EXTERNAL_LINKS['/learn']} target="_blank" rel="noopener noreferrer">
-              LEARN
-            </a>
-          </Menu.Item>
+          <Menu.SubMenu title="LEARN" onTitleClick={() => window.open(EXTERNAL_LINKS['/learn'], '_blank')}>
+            <Menu.Item key="/add-market">
+              <a href={EXTERNAL_LINKS['/add-market']} target="_blank" rel="noopener noreferrer">
+                Adding a market
+              </a>
+            </Menu.Item>
+            <Menu.Item key="/wallet-support">
+              <a href={EXTERNAL_LINKS['/wallet-support']} target="_blank" rel="noopener noreferrer">
+                Supported wallets
+              </a>
+            </Menu.Item>
+            <Menu.Item key="/dex-list">
+              <a href={EXTERNAL_LINKS['/dex-list']} target="_blank" rel="noopener noreferrer">
+                DEX list
+              </a>
+            </Menu.Item>
+            <Menu.Item key="/developer-resources">
+              <a href={EXTERNAL_LINKS['/developer-resources']} target="_blank" rel="noopener noreferrer">
+                Developer resources
+              </a>
+            </Menu.Item>
+            <Menu.Item key="/explorer">
+              <a href={EXTERNAL_LINKS['/explorer']} target="_blank" rel="noopener noreferrer">
+                Solana block explorer
+              </a>
+            </Menu.Item>
+            <Menu.Item key="/srm-faq">
+              <a href={EXTERNAL_LINKS['/srm-faq']} target="_blank" rel="noopener noreferrer">
+                SRM FAQ
+              </a>
+            </Menu.Item>
+          </Menu.SubMenu>
         </Menu>
         <div>
           <Row
