99fbbed7d5398bad18f227daac35d2e9b1247aca
Nishad <nishadsingh@gmail.com>
2020-10-22 00:04:11 +0800

    Transition to new USDC markets

--- src/components/DeprecatedMarketsInstructions.js
+++ src/components/DeprecatedMarketsInstructions.js
@@ -8,6 +8,7 @@ import {
 import FloatingElement from './layout/FloatingElement';
 import CheckOutlined from '@ant-design/icons/lib/icons/CheckOutlined';
 import BalancesTable from './UserInfoTable/BalancesTable';
+import Link from './Link';
 import OpenOrderTable from './UserInfoTable/OpenOrderTable';
 import SyncOutlined from '@ant-design/icons/lib/icons/SyncOutlined';
 
@@ -30,16 +31,55 @@ export default function DeprecatedMarketsInstructions({ switchToLiveMarkets }) {
   return (
     <FloatingElement>
       <Title level={4} style={{ color: 'rgba(255, 255, 255, 1)' }}>
-        Migrate to new markets
+        Migrate to new USDC markets
       </Title>
       <Typography>
-        Serum has rolled out upgraded markets! They're faster and support fee
-        discounts based on SRM holdings.
+        <Link
+          external
+          to={
+            'https://explorer.solana.com/address/EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v'
+          }
+        >
+          Native USDC
+        </Link>{' '}
+        has arrived on Solana! See{' '}
+        <Link
+          external
+          to={
+            'https://www.circle.com/blog/circle-partners-with-alameda-research-ftx-project-serum-to-introduce-support-for-usdc-on-solana'
+          }
+        >
+          Circle's announcement
+        </Link>{' '}
+        for more information.
       </Typography>
       <br />
       <Typography>
-        To migrate over to the new markets, please cancel your orders and settle
-        your funds on old markets.
+        Serum is transitioning from the previous{' '}
+        <Link
+          external
+          to={
+            'https://explorer.solana.com/address/BXXkv6z8ykpG1yuvUDPgh732wzVHB69RnB9YgSYh3itW'
+          }
+        >
+          Wrapped USDC
+        </Link>
+        , now called WUSDC, to Native USDC. You can convert from WUSDC to USDC
+        directly on{' '}
+        <Link external to={'https://sollet.io'}>
+          sollet.io
+        </Link>
+        , or can deposit WUSDC and be credited USDC on exchanges like{' '}
+        <Link external to={'https://ftx.com'}>
+          FTX
+        </Link>
+        .
+      </Typography>
+      <br />
+      <Typography>
+        Markets with WUSDC as the quote curency are now deprecated. To migrate
+        over to the new USDC markets, please cancel your orders and settle your
+        funds on old markets.
       </Typography>
       <div style={{ marginTop: 20, display: 'flex', justifyContent: 'center' }}>
         <Button onClick={() => refresh(true)}>
