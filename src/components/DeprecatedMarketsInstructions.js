da46a2f7c6bc6fd13f3d051e44df8a8744729b07
Sebastian Conybeare <sebastian@projectserum.com>
2021-02-28 06:01:21 +0800

    Prune import

--- src/components/DeprecatedMarketsInstructions.js
+++ src/components/DeprecatedMarketsInstructions.js
@@ -8,7 +8,6 @@ import {
 import FloatingElement from './layout/FloatingElement';
 import CheckOutlined from '@ant-design/icons/lib/icons/CheckOutlined';
 import BalancesTable from './UserInfoTable/BalancesTable';
-import Link from './Link';
 import OpenOrderTable from './UserInfoTable/OpenOrderTable';
 import SyncOutlined from '@ant-design/icons/lib/icons/SyncOutlined';
 
