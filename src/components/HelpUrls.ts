bc675acfc9c9118269c900be05680f495b0b0ee1
KermitSwap <kermitswap@protonmail.com>
2021-05-23 00:38:52 +0530

    updated twitter

--- src/components/HelpUrls.ts
+++ src/components/HelpUrls.ts
@@ -1,8 +1,8 @@
 export const helpUrls = {
-  customerSupport: 'https://twitter.com/CoinKermit',
-  customerSupportZh: 'https://twitter.com/CoinKermitt',
+  customerSupport: 'https://twitter.com/KermitFinance',
+  customerSupportZh: 'https://twitter.com/KermitFinance',
   contactEmail: 'mailto:contact@kermitoken@gmail.com',
-  twitter: 'https://twitter.com/CoinKermit',
+  twitter: 'https://twitter.com/KermitFinance',
   discord: 'https://discord.gg/kermit',
   home: 'https://www.kermitcoin.xyz',
   subreddit: 'https://www.reddit.com/r/kermitcoin/',
