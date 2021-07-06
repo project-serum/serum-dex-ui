0b8e13c4db9721d2cc7f8b1b1fa857bd3f9b51ae
Nathaniel Parke <nathaniel.parke@gmail.com>
2021-07-06 21:05:24 +0800

    Updates

---
+++ precache-manifest.968158b433b254636a30e1e250d8704a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfa071a0a6892b17ee7aafffe46a11f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c2ebd63e276ff5905bd",
+    "url": "/static/css/2.3a0ac4c9.chunk.css"
+  },
+  {
+    "revision": "0081e78a5e3a683d4d60",
+    "url": "/static/css/main.0d007200.chunk.css"
+  },
+  {
+    "revision": "2c2ebd63e276ff5905bd",
+    "url": "/static/js/2.1c20c804.chunk.js"
+  },
+  {
+    "revision": "c84ff2ee1e84300b54bc9f2005e87aef",
+    "url": "/static/js/2.1c20c804.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0081e78a5e3a683d4d60",
+    "url": "/static/js/main.c94de8d0.chunk.js"
+  },
+  {
+    "revision": "d3ebb56a3098223b5316",
+    "url": "/static/js/runtime-main.c7319a82.js"
+  },
+  {
+    "revision": "49174c736a9c871baaeb2db6d5372eee",
+    "url": "/static/media/logo.49174c73.svg"
+  }
+]);
