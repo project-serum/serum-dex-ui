97d0720a7fe81924e9dd7be6a075aa731d7eb798
0xDEADBEEF <0x@fsynth.io>
2021-08-31 16:41:03 +0100

    Removed broken charts

--- src/pages/TradePage.tsx
+++ src/pages/TradePage.tsx
@@ -25,7 +25,6 @@ import CustomMarketDialog from '../components/CustomMarketDialog';
 import { notify } from '../utils/notifications';
 import { useHistory, useParams } from 'react-router-dom';
 import { nanoid } from 'nanoid';
-import { TVChartContainer } from '../components/TradingView';
 
 const { Option, OptGroup } = Select;
 
@@ -331,13 +330,6 @@ const DeprecatedMarketsPage = ({ switchToLiveMarkets }) => {
 const RenderNormal = ({ onChangeOrderRef, onPrice, onSize }) => {
   return (
     <>
-    <Row>
-      <Col xs={24} sm={24} style={{ height: '100%', display: 'flex', minHeight: '400px' }}>
-        <div style={{ width: '-webkit-fill-available', display: 'flex' }}>
-          <TVChartContainer />
-        </div>
-      </Col>
-    </Row>
     <Row
       style={{
         minHeight: '900px',
@@ -366,13 +358,6 @@ const RenderNormal = ({ onChangeOrderRef, onPrice, onSize }) => {
 const RenderSmall = ({ onChangeOrderRef, onPrice, onSize }) => {
   return (
     <>
-      <Row>
-        <Col xs={24} sm={24} style={{ height: '100%', display: 'flex', minHeight: '400px' }}>
-          <div style={{ width: '-webkit-fill-available', display: 'flex' }}>
-            <TVChartContainer />
-          </div>
-        </Col>
-      </Row>
       <Row
         style={{
           height: '900px',
@@ -409,13 +394,6 @@ const RenderSmall = ({ onChangeOrderRef, onPrice, onSize }) => {
 const RenderSmaller = ({ onChangeOrderRef, onPrice, onSize }) => {
   return (
     <>
-      <Row>
-        <Col xs={24} sm={24} style={{ height: '100%', display: 'flex', minHeight: '300px' }}>
-          <div style={{ width: '-webkit-fill-available', display: 'flex' }}>
-            <TVChartContainer />
-          </div>
-        </Col>
-      </Row>
       <Row>
         <Col xs={24} sm={12} style={{ height: '100%', display: 'flex' }}>
           <TradeForm style={{ flex: 1 }} setChangeOrderRef={onChangeOrderRef} />
