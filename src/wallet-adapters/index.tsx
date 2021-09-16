b205fda523c83fb29afb6906cc3559b9c0b18394
bitkeepwallet <85662627+bitkeepwallet@users.noreply.github.com>
2021-09-16 15:57:27 +0800

    Update index.tsx

--- src/wallet-adapters/index.tsx
+++ src/wallet-adapters/index.tsx
@@ -2,5 +2,6 @@ export * from './ledger';
 export * from './phantom';
 export * from './sollet-extension';
 export * from './math';
+export * from './bitkeep';
 export * from './types';
 export * from './solflare-extension';
