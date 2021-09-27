f35f60df67144aca1987dcc85c7a0b91ea0a320d
daHIFI <el@homeboy.wade.internal>
2021-09-27 20:29:51 +0000

    fixed transparency

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -20,7 +20,7 @@ import AppSearch from './AppSearch';
 import { getTradePageUrl } from '../utils/markets';
 
 const Wrapper = styled.div`
-  background-color: #0d1017;
+  //background-color: #0d1017;
   display: flex;
   flex-direction: row;
   justify-content: flex-end;
