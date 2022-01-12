e70ff3adc249abf556481056ec31291ed6ce00a1
0xlook <47668701+0xlook@users.noreply.github.com>
2022-01-12 22:29:33 +1000

    Rename IPoolCreateRedeemPanel

--- src/pages/pools/PoolPage/PoolCreateRedeemPanel.tsx
+++ src/pages/pools/PoolPage/PoolCreateRedeemPanel.tsx
@@ -17,7 +17,7 @@ import { sendTransaction } from '../../../utils/send';
 const { Text } = Typography;
 const { TabPane } = Tabs;
 
-interface PoolCreateRedeemPanel {
+interface IPoolCreateRedeemPanel {
   poolInfo: PoolInfo;
   mintInfo: MintInfo;
 }
@@ -25,7 +25,7 @@ interface PoolCreateRedeemPanel {
 export default function PoolCreateRedeemPanel({
   poolInfo,
   mintInfo,
-}: PoolCreateRedeemPanel) {
+}: IPoolCreateRedeemPanel) {
   return (
     <FloatingElement stretchVertical>
       <Tabs>
