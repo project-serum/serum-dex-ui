88b1b5c3902730a92e8d0f0f583ca78a05eb0e0a
jhlx <jay@alameda-research.com>
2022-10-22 04:27:37 +0800

    Update build system

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,13 @@
 const CracoLessPlugin = require('craco-less');
 
 module.exports = {
+  babel: {
+    plugins: [
+      '@babel/plugin-proposal-nullish-coalescing-operator',
+      '@babel/plugin-proposal-optional-chaining',
+      '@babel/plugin-transform-shorthand-properties',
+    ],
+  },
   plugins: [
     {
       plugin: CracoLessPlugin,
